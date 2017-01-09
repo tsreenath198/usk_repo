fc8e35fb3b5ed2273f91bec85640fba419947a48
anuraguskcorp <anurag.uskcorp@gmail.com>
2017-01-09 16:30:05 +0530

    Employee Attendance Controller

--- dmt_rest_service/src/main/java/in/uskcorp/tool/dmt/controller/EmployeeAttendanceController.java
+++ dmt_rest_service/src/main/java/in/uskcorp/tool/dmt/controller/EmployeeAttendanceController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(DMTRestURIConstants.EMPLOYEEATTENDANCE)
+@RequestMapping(DMTRestURIConstants.EMPLOYEE_ATTENDANCE)
 public class EmployeeAttendanceController extends APIController<EmployeeAttendance> {
 	@Autowired
 	@Qualifier("employeeAttendenceServiceImpl")

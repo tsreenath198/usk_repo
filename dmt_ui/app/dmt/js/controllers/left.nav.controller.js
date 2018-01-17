e12c7171c311bcd85c05cf4c95a85dd0af832f5e
vickyuskcorp <vicky.uskcorp@gmail.com>
2018-01-17 10:44:20 +0530

    export changes

--- dmt_ui/app/dmt/js/controllers/left.nav.controller.js
+++ dmt_ui/app/dmt/js/controllers/left.nav.controller.js
@@ -12,14 +12,13 @@ function leftNavController($scope, leftNavService, Excel, $state, $mdDialog,
       $scope.toggleSearch = function(){
       	$scope.toggleStart = ($scope.toggleStart == false)?true:false;
       };      
-      $scope.exportData = function (tableId) {
-        // $scope.tasksOptions = [ $scope.tasksData.length ];
-        var exportHref = Excel.tableToExcel(tableId, 'sheet name');
+      $scope.exportData = function () {
+        var exportTable = "#"+$scope.tableToExport+"Table";
+        var exportHref = Excel.tableToExcel(exportTable, 'sheet name');
         $timeout(function () {
             location.href = exportHref;
         }, 100); // trigger download
     }
-
       $scope.toggle = function(itemPos) {
     if ($scope.menuIsOpen === itemPos) {
       $scope.menuIsOpen = 0;
@@ -191,8 +190,55 @@ function leftNavController($scope, leftNavService, Excel, $state, $mdDialog,
 			leftNavService.getAllTabs().then(function(response) {
 			$scope.leftTabs = response.data;
     });
-    var tableId = document.getElementsByTagName("table");
-    console.log(tableId);
+     $scope.tablesToExcel= function (){
+    var uri = 'data:application/vnd.ms-excel;base64,'
+    , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets>'
+    , templateend = '</x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head>'
+    , body = '<body>'
+    , tablevar = '<table>{table'
+    , tablevarend = '}</table>'
+    , bodyend = '</body></html>'
+    , worksheet = '<x:ExcelWorksheet><x:Name>'
+    , worksheetend = '</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet>'
+    , worksheetvar = '{worksheet'
+    , worksheetvarend = '}'
+    , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+    , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
+    , wstemplate = ''
+    , tabletemplate = '';
+
+    return function (table, name, filename) {
+        var tables = table;
+
+        for (var i = 0; i < tables.length; ++i) {
+            wstemplate += worksheet + worksheetvar + i + worksheetvarend + worksheetend;
+            tabletemplate += tablevar + i + tablevarend;
+        }
+
+        var allTemplate = template + wstemplate + templateend;
+        var allWorksheet = body + tabletemplate + bodyend;
+        var allOfIt = allTemplate + allWorksheet;
+
+        var ctx = {};
+        for (var j = 0; j < tables.length; ++j) {
+            ctx['worksheet' + j] = name[j];
+        }
+
+        for (var k = 0; k < tables.length; ++k) {
+            var exceltable;
+            if (!tables[k].nodeType) exceltable = document.getElementById(tables[k]);
+            ctx['table' + k] = exceltable.innerHTML;
+        }
+
+        //document.getElementById("dlink").href = uri + base64(format(template, ctx));
+        //document.getElementById("dlink").download = filename;
+        //document.getElementById("dlink").click();
+
+        window.location.href = uri + base64(format(allOfIt, ctx));
+
+    }
+}();
+  
 	}
 
 	function buildToggler(componentId) {
@@ -203,6 +249,10 @@ function leftNavController($scope, leftNavService, Excel, $state, $mdDialog,
         $scope.init = self.init;
         $scope.toggleNavigationLeft = buildToggler('left');
 	init();
+
+    var str = $state.current.url;
+    str = str.replace( /[/]/g, "" );
+    $scope.tableToExport = str;
 
 	return self;
 };

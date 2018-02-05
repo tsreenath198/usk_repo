1a7200c2009d9d970d2943e5a6d65c56585aaf5b
achyuthkumar <achyuth.uskcorp@gmail.com>
2018-02-05 18:40:47 +0530

    login backend

--- dmt_rest_service/src/main/java/in/uskcorp/tool/dmt/service/UserDetailsService.java
+++ dmt_rest_service/src/main/java/in/uskcorp/tool/dmt/service/UserDetailsService.java
@@ -2,6 +2,6 @@ package in.uskcorp.tool.dmt.service;
 
 import in.uskcorp.tool.dmt.domain.UserDetails;
 
-public abstract class UserDetailsService extends APIService<UserDetails>{
-	public abstract UserDetails readByValues(String username,String password);
+public abstract class UserDetailsService extends APIService<UserDetails> {
+	public abstract boolean readByValues(UserDetails userDetails);
 }

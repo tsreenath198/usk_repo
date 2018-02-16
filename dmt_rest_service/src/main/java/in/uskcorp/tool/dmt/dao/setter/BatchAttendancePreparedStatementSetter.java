300535086eb07cd105f47100888361fe43bb59b7
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2018-02-16 16:36:52 +0530

    In this screen i added trainee name

--- dmt_rest_service/src/main/java/in/uskcorp/tool/dmt/dao/setter/BatchAttendancePreparedStatementSetter.java
+++ dmt_rest_service/src/main/java/in/uskcorp/tool/dmt/dao/setter/BatchAttendancePreparedStatementSetter.java
@@ -24,7 +24,7 @@ public class BatchAttendancePreparedStatementSetter implements
 	public void setValues(PreparedStatement arg0) throws SQLException {
 		arg0.setInt(1, batchAttendance.getBatchId());
 		arg0.setDate(2, ResultSetUtil.converttoSQLDate(new Date()));
-		//arg0.setInt(3, batchAttendance.getTraineeId());
+		arg0.setInt(3, batchAttendance.getTraineeId());
 		arg0.setDate(4, ResultSetUtil.converttoSQLDate(new Date()));
 		arg0.setString(5, batchAttendance.getDescription());
 		if (!isInsert) {

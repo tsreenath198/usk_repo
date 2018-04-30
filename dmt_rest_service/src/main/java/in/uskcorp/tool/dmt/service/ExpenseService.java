dab271e446f533c7b7ad65e525c6f6a63bdc094c
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2018-04-30 13:26:27 +0530

    Modified as per requirement

--- dmt_rest_service/src/main/java/in/uskcorp/tool/dmt/service/ExpenseService.java
+++ dmt_rest_service/src/main/java/in/uskcorp/tool/dmt/service/ExpenseService.java
@@ -3,4 +3,6 @@ package in.uskcorp.tool.dmt.service;
 import in.uskcorp.tool.dmt.domain.Expense;
 
 public abstract class ExpenseService extends APIService<Expense> {
+	public abstract void insert(Expense e) throws Exception;
+	public abstract void edit(Expense e) throws Exception ;
 }
